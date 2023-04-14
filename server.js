f212682ccc26ac266a218862ddd07c1b8bebc721
KPanuwat-S <panuwatbood@gmail.com>
2023-04-15 00:14:46 +0700

    Complete API-CRUD Controller

--- server.js
+++ server.js
@@ -19,34 +19,6 @@ mongoose
     console.log(`Success connection`);
   });
 
-const tourSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price.'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-console.log('Create Schema');
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 997,
-});
-
-testTour
-  .save()
-  .then((doc) => console.log(doc))
-  .catch((err) => console.log(`Error: ${err}`));
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
